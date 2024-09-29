b6ae3d5777241e1c20d12bdb2ddab21efae52a9c
Fleur Slabbinck <fleur.slabbinck@gmail.com>
2024-09-29 12:15:41 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+
+window.onload = function () {
+    var iframe = document.getElementById('youtube-video');
+    iframe.src = iframe.src; // This forces the iframe to reload its content
+}
