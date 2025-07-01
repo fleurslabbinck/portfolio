b297389d34664ec04d17e222efde827d8300671a
Fleur Slabbinck <fleur.slabbinck@gmail.com>
2025-07-01 13:31:09 +0200

    update: updated and refactored

--- script.js
+++
@@ -1,6 +0,0 @@
-// JavaScript source code
-
-window.onload = function () {
-    var iframe = document.getElementById('youtube-video');
-    iframe.src = iframe.src; // This forces the iframe to reload its content
-}
